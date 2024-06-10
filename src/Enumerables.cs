17db99040606de0effc8e2e3339e7643fe56f0a6
meep.tech <meepdottech@gmail.com>
2024-06-10 19:37:52 -0400

    Cleanup and docs

--- src/Enumerables.cs
+++ src/Enumerables.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 
 namespace Meep.Tech.Collections {
+
+    /// <summary>
+    /// Common Extensions for IEnumerable types.
+    /// </summary>
     public static class EnumerableExtensions {
 
         #region ForEach
 
+        /// <summary>
+        /// Used to iterate over an IEnumerable and perform an action on each element without returning a value.
+        /// </summary>
+        /// <returns>The original IEnumerable for chaining.</returns>
         public static IEnumerable<TType> ForEach<TType>(this IEnumerable<TType> source, Action<TType> action) {
             foreach(TType? item in source) {
                 action(item);
@@ -13,6 +21,7 @@ namespace Meep.Tech.Collections {
             return source;
         }
 
+        /// <inheritdoc cref="ForEach{TType}(IEnumerable{TType}, Action{TType})"/>
         public static IEnumerable<TType> ForEach<TType>(this IEnumerable<TType> source, Action<TType, int> action) {
             int index = 0;
             foreach(TType? item in source) {
@@ -22,6 +31,7 @@ namespace Meep.Tech.Collections {
             return source;
         }
 
+        /// <inheritdoc cref="ForEach{TType}(IEnumerable{TType}, Action{TType})"/>
         public static IEnumerable<TType> ForEach<TType, TDiscard>(this IEnumerable<TType> source, Func<TType, TDiscard> action) {
             foreach(TType? item in source) {
                 _ = action(item);
@@ -30,6 +40,7 @@ namespace Meep.Tech.Collections {
             return source;
         }
 
+        /// <inheritdoc cref="ForEach{TType}(IEnumerable{TType}, Action{TType})"/>
         public static IEnumerable<TType> ForEach<TType, TDiscard>(this IEnumerable<TType> source, Func<TType, int, TDiscard> action) {
             int index = 0;
             foreach(TType? item in source) {
@@ -43,6 +54,7 @@ namespace Meep.Tech.Collections {
 
         #region First [or Default]
 
+        /// <inheritdoc cref="Enumerable.First{TSource}(IEnumerable{TSource}, Func{TSource, bool})"/>
         public static object? First(this IEnumerable source, Func<object, bool> predicate) {
             foreach(object? item in source) {
                 if(predicate(item)) {
@@ -53,14 +65,15 @@ namespace Meep.Tech.Collections {
             throw new InvalidOperationException("No matching element found.");
         }
 
-        public static object? FirstOrDefault(this IEnumerable source, Func<object, bool> predicate) {
+        /// <inheritdoc cref="Enumerable.FirstOrDefault{TSource}(IEnumerable{TSource}, Func{TSource, bool})"/>
+        public static object? FirstOrDefault(this IEnumerable source, Func<object, bool> predicate, object? defaultValue = null) {
             foreach(object? item in source) {
                 if(predicate(item)) {
                     return item;
                 }
             }
 
-            return null;
+            return defaultValue;
         }
 
         #endregion
--- src/Lists.cs
+++ src/Lists.cs
@@ -1,7 +1,16 @@
 using System.Collections;
 
 namespace Meep.Tech.Collections {
+
+  /// <summary>
+  /// Common Extensions for List types.
+  /// </summary>
   public static class ListExtensions {
+
+    /// <summary>
+    /// Used to check if a list is long enough before accessing an index.
+    /// </summary>
+    /// <returns>True if the index is within the list bounds, false otherwise.</returns>
     public static bool TryGetValueAt<T>(this IReadOnlyList<T> list, int index, out T value) {
       if(index >= 0 && index < list.Count) {
         value = list[index];
@@ -12,6 +21,7 @@ namespace Meep.Tech.Collections {
       return false;
     }
 
+    /// <inheritdoc cref="TryGetValueAt{T}(IReadOnlyList{T}, int, out T)"/>
     public static bool TryGetValueAt(this IList list, int index, out object? value) {
       if(index >= 0 && index < list.Count) {
         value = list[index];
